683b615c5409a4028cc9ebf24e497e1d9bb58487
Wadim Dziedzic <nikdo@aviary.pl>
2008-05-05 18:10:52 +0000

    mindmap

---
+++ GNOME PL.mm
@@ -0,0 +1,105 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1209827901218" ID="Freemind_Link_1793516411" MODIFIED="1210010738265" TEXT="GNOME PL">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1209827916187" ID="_" MODIFIED="1209831280703" POSITION="right" TEXT="Kontakt" VSHIFT="-19">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1209828257046" HGAP="26" ID="Freemind_Link_1008596290" MODIFIED="1209831340828" TEXT="e-mail/alias do kontaktu" VSHIFT="29">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209827944531" HGAP="21" ID="Freemind_Link_920923945" MODIFIED="1209831445937" TEXT="strona" VSHIFT="5">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1209828176859" ID="Freemind_Link_1984237106" MODIFIED="1209829993578" TEXT="domena" VSHIFT="-14">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="help"/>
+<node COLOR="#111111" CREATED="1209827962515" ID="Freemind_Link_1105701758" MODIFIED="1209829979984" TEXT="gnomepl.org -utracone" VSHIFT="-9">
+<icon BUILTIN="help"/>
+</node>
+<node COLOR="#111111" CREATED="1209827979906" ID="Freemind_Link_1584497854" MODIFIED="1209830000203" TEXT="status gnome.org">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1209828209734" ID="Freemind_Link_1039595883" MODIFIED="1209828214421" TEXT="zawarto&#x15b;&#x107;">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1209829949609" HGAP="21" ID="Freemind_Link_1110974195" MODIFIED="1209829957250" TEXT="kontakt" VSHIFT="4"/>
+<node COLOR="#111111" CREATED="1209828219390" ID="Freemind_Link_290170421" MODIFIED="1209829309031" TEXT="formularz b&#x142;&#x119;d&#xf3;w"/>
+<node COLOR="#111111" CREATED="1209828230734" ID="Freemind_Link_85752275" MODIFIED="1209831421171" TEXT="news blog planeta">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1209828365921" ID="Freemind_Link_1574071976" MODIFIED="1210010736265" POSITION="right" TEXT="U&#x17c;ytkownicy" VSHIFT="-24">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1209828371718" ID="Freemind_Link_1739349032" MODIFIED="1209828535515" TEXT="feedback">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209828902046" ID="Freemind_Link_1625479027" MODIFIED="1209829052593" TEXT="spo&#x142;eczno&#x15b;&#x107;">
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="help"/>
+<node COLOR="#990000" CREATED="1209829317718" ID="Freemind_Link_1555067480" MODIFIED="1209831392437" TEXT="fora ubuntu/suse" VSHIFT="26">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1209828314734" ID="Freemind_Link_845373380" MODIFIED="1209831358875" POSITION="left" TEXT="T&#x142;umaczenia" VSHIFT="5">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1209828328593" ID="Freemind_Link_1785106007" MODIFIED="1209828337375" TEXT="jako&#x15b;&#x107;">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1209828494578" ID="Freemind_Link_656305047" MODIFIED="1209831403859" TEXT="peer review">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1209828504031" ID="Freemind_Link_409389977" MODIFIED="1210010127046" TEXT="testowanie &quot;live&quot;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1209829062656" ID="Freemind_Link_498213494" MODIFIED="1209829066046" TEXT="bugzilla">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1209828338375" HGAP="22" ID="Freemind_Link_970924570" MODIFIED="1209831471171" TEXT="s&#x142;ownik" VSHIFT="-7">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1209828401500" ID="Freemind_Link_1312324604" MODIFIED="1210010097234" TEXT="ujednolicenia">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1209828632281" ID="Freemind_Link_526161452" MODIFIED="1209831343843" TEXT="credits"/>
+<node COLOR="#111111" CREATED="1209828644437" ID="Freemind_Link_201690780" MODIFIED="1210010738875" TEXT="preferencje/opcje/ustawienia"/>
+<node COLOR="#111111" CREATED="1209828792875" ID="Freemind_Link_1528177593" MODIFIED="1209831375828" TEXT="+inne na b.a.p"/>
+</node>
+<node COLOR="#990000" CREATED="1209828405968" ID="Freemind_Link_1935300084" MODIFIED="1209828823453" TEXT="wykorzystanie GWO">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1210010747468" ID="Freemind_Link_382337961" MODIFIED="1210010762515" TEXT="polska terminologia &quot;accesibility&quot;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1209828471015" ID="Freemind_Link_1920310955" MODIFIED="1209828839375" TEXT="aplikacje">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1209828840078" ID="Freemind_Link_242476015" MODIFIED="1209828949531" TEXT="lista priorytet&#xf3;w">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1209828845531" ID="Freemind_Link_1904113311" MODIFIED="1209829258781" TEXT="pomoc">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1209828462968" HGAP="42" ID="Freemind_Link_1990546122" MODIFIED="1210010735843" POSITION="left" TEXT="Team" VSHIFT="-48">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1209828487953" ID="Freemind_Link_702578833" MODIFIED="1209829266109" TEXT="rozw&#xf3;j">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209828869406" ID="Freemind_Link_590655761" MODIFIED="1209829266109" TEXT="role">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1209828874234" ID="Freemind_Link_755588663" MODIFIED="1209829266109" TEXT="QA manager">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="help"/>
+</node>
+<node COLOR="#990000" CREATED="1209828889765" ID="Freemind_Link_55648056" MODIFIED="1209829266125" TEXT="opiekunowie aplikacji">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+</node>
+</map>
